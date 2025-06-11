5855de025df91356681cbbb1a83a668cfde3d70a
Erdal <erdaltasar24@gmail.com>
2025-06-11 16:22:36 +0300

    render update

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ python manage.py migrate
 # Verileri yükleme (eğer migration başarılı olursa ve veritabanı boşsa)
 if [ -f initial_data.json ]; then
     echo "Veritabanı verilerini yüklüyorum..."
-    python manage.py loaddata initial_data.json
+    # Önce dosyayı UTF-8'e dönüştürelim
+    iconv -f ISO-8859-9 -t UTF-8 initial_data.json > initial_data_utf8.json
+    python manage.py loaddata initial_data_utf8.json
 fi
 
 # Statik dosyaları toplama

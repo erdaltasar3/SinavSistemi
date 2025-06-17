078aaf89066fca0008dc0ec2aaa75ffff635afc0
Erdal <erdaltasar24@gmail.com>
2025-06-17 03:13:13 +0300

    sınav ekranı güncellemeleri

--- SinavSistemi/settings.py
+++ SinavSistemi/settings.py
@@ -49,8 +49,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'True'
-# DEBUG = get_secret('DEBUG') == 'True'
+DEBUG = get_secret('DEBUG') == 'True'
 
 ALLOWED_HOSTS = get_secret('ALLOWED_HOSTS').split(',')
 
